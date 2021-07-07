36e9420bfe04e41cc464cc8d45601545e2e719fd
souzasam <spksouza@hotmail.com>
2021-07-07 13:49:27 -0300

    settings

--- R/set_env.R
+++ R/set_env.R
@@ -13,9 +13,9 @@ set_env = function () {
 
   reticulate::virtualenv_create(envname = '~/.env',
                                 python = NULL,
+                                version = '3.8.7',
                                 packages = c('numpy','pandas','selenium',
-                                             'webdrivermanager'))
-
+                                             'webdriver_manager'))
   Sys.setenv(RETICULATE_PYTHON='~/.env/Scripts')
 
 }
