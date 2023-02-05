74a1b682c6888847ede7000c9a95d489c09ffd07
samuel-souza <spksouza@hotmail.com>
2023-02-05 04:34:09 -0300

    Manutenção 1

--- R/set_env.R
+++ R/set_env.R
@@ -20,6 +20,8 @@ set_env = function () {
 
   if (os == "Windows") {
     Sys.setenv(RETICULATE_PYTHON='~/.env/Scripts')
+    reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+                                   ignore_installed = T)
   } else {
     Sys.setenv(RETICULATE_PYTHON='~/.env/bin')
     reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
