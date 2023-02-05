13b52a94e2c5f4cc2d80898a8676fe2ee5d81d0e
samuel-souza <spksouza@hotmail.com>
2023-02-05 19:26:56 -0300

    Manutenção 2

--- R/set_env.R
+++ R/set_env.R
@@ -9,22 +9,46 @@
 #' @examples set_env()
 
 
+# set_env = function () {
+
+#   reticulate::virtualenv_create(envname = '~/.env',
+#                                 python = NULL,
+#                                 version = '3.8.7',
+#                                 packages=c('numpy','pandas','selenium'),
+#                                 pip_options = character('python3 -m pip install --upgrade pip'))
+#   os = Sys.info()[1]
+
+#   if (os == "Windows") {
+#     Sys.setenv(RETICULATE_PYTHON='~/.env/Scripts')
+#     reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+#                                    ignore_installed = T)
+#   } else {
+#     Sys.setenv(RETICULATE_PYTHON='~/.env/bin')
+#     reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+#                                    ignore_installed = T)
+#   }
+
+# }
+
+########################################################################################################
+
 set_env = function () {
 
-  reticulate::virtualenv_create(envname = '~/.env',
+  reticulate::virtualenv_create(envname = paste0(path.package('soccerdatas'),'/.env'),
                                 python = NULL,
                                 version = '3.8.7',
                                 packages=c('numpy','pandas','selenium'),
                                 pip_options = character('python3 -m pip install --upgrade pip'))
+
   os = Sys.info()[1]
 
   if (os == "Windows") {
-    Sys.setenv(RETICULATE_PYTHON='~/.env/Scripts')
-   reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+    Sys.setenv(RETICULATE_PYTHON=paste0(path.package('soccerdatas'),'/.env/Scripts')
+    reticulate::virtualenv_install(envname = paste0(path.package('soccerdatas'),'/.env', packages = "webdriver-manager",
                                    ignore_installed = T)
   } else {
-    Sys.setenv(RETICULATE_PYTHON='~/.env/bin')
-    reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+    Sys.setenv(RETICULATE_PYTHON=paste0(path.package('soccerdatas'),'/.env/bin')
+    reticulate::virtualenv_install(envname = paste0(path.package('soccerdatas'),'/.env', packages = "webdriver-manager",
                                    ignore_installed = T)
   }
 
