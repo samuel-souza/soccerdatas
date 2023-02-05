8c2bb4058c9fb55857a0de233488c02126bd534e
samuel-souza <spksouza@hotmail.com>
2023-02-05 05:14:15 -0300

    1

--- R/set_env.R
+++ R/set_env.R
@@ -13,19 +13,19 @@ set_env = function () {
 
   reticulate::virtualenv_create(envname = '~/.env',
                                 python = NULL,
-                             #   version = '3.8.7',
-                                packages=c('numpy','pandas','selenium','webdriver-manager'),
+                                version = '3.8.7',
+                                packages=c('numpy','pandas','selenium'),
                                 pip_options = character('python3 -m pip install --upgrade pip'))
   os = Sys.info()[1]
 
   if (os == "Windows") {
     Sys.setenv(RETICULATE_PYTHON='~/.env/Scripts')
-#   reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
-#                                   ignore_installed = T)
+   reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+                                   ignore_installed = T)
   } else {
     Sys.setenv(RETICULATE_PYTHON='~/.env/bin')
-#    reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
-#                                   ignore_installed = T)
+    reticulate::virtualenv_install(envname = "~/.env", packages = "webdriver-manager",
+                                   ignore_installed = T)
   }
 
 }
